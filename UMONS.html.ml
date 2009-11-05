7df126ae65dbc1d40421f0531167065e9557069e
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2009-11-05 19:12:44 +0100

    Implementation of the navigation bar (without links, so far).  Function to set the "toolbar" links according to the language.

--- UMONS.html.ml
+++ UMONS.html.ml
@@ -1,17 +1,40 @@
 (* Specific functions for the UMONS template. *)
 
+open Printf
 open Nethtml
 
-let separation ~url_base =
+let separation url_base =
   Element("span", ["class", "separation"],
           [Element("img", ["src", url_base ^ "images/right_arrow.png"], [])])
 
-(* Add a separation in front of each element (there is already a
-   prefix in the navigation bar => 1st element also needs a sep. *)
-let rec add_separation sep l = match l with
-  | [] -> l
-  | a :: tl -> sep :: a :: add_separation sep tl
+(* All paths start with "." which stands for the institute. *)
+let rec transform_path institut sep p = match p with
+  | [] -> []
+  | a :: tl ->
+      let a = if a = "." then institut else String.capitalize a in
+      sep :: Data(a) :: transform_path institut sep tl
 
-let navigation_bar tpl ~url_base items =
-  let sep = separation ~url_base in
-  navigation_bar tpl (add_separation sep items)
+let navigation_of_path tpl rel_path fname =
+  let institut = Get.title tpl in
+  let sep = separation (Get.url_base tpl) in
+  let p = Neturl.split_path rel_path in
+  let p = (if fname = "index.html" || fname = "index.htm" then p
+           else p @ [try Filename.chop_extension fname with _ -> fname]) in
+  navigation_bar tpl (transform_path institut sep p)
+
+
+
+let toolbar l =
+  let tpl = lang empty l in
+  let admin = sprintf "http://portail.umons.ac.be/%s/universite/admin" l in
+  let tpl = url_annuaire tpl
+    "http://telephone.umh.ac.be/reppersumons/REPPERSlist.asp" in
+  let tpl = url_biblio tpl "http://w3.umh.ac.be/Bibli/sms.htm" in
+  let tpl = url_elearning tpl
+    "https://applications.umons.ac.be/moodleumh/course/category.php?id=2" in
+  let tpl = url_plan tpl
+    (admin ^ "/scrp/plancampus/Pages/CampusSciencesMédecineFTI-EII.aspx") in
+  let tpl = url_emploi tpl (admin ^ "/drh/emploi/Pages/Emploi.aspx") in
+  let tpl = url_agenda tpl (admin ^ "/scrp/Pages/Agenda.aspx") in
+  tpl
+
--- UMONS.html.mli
+++ UMONS.html.mli
@@ -1,6 +1,12 @@
 (** Some utilities related to the UMONS template. *)
 
+val toolbar : string -> t
+  (** [toobar lang] returns a template with the links of the toolbar
+      ("Annuaire",...) filled with the appropriate links according to
+      the language [lang] (must be "fr" or "en" or any other
+      abbreviation of the main university site). *)
+
 (* @hide navigation_bar *)
-val navigation_bar : t -> url_base:string -> Nethtml.document list -> t
-  (** [navigation_bar t ~url_base items] construct the navigation bar
-      from the list of [items]. *)
+val navigation_of_path : t -> string -> string -> t
+  (** [navigation_of_path t rel_path fname] add a navigation bar based
+      on the path [rel_path]/[fname]. *)
